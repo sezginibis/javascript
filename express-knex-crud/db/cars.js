0a62d18496cfabbaa26173fd24ec184d9e054237
Sezgin İbiş (incelemeelemani) <34094261+sezginibis@users.noreply.github.com>
2022-03-27 11:58:59 +0300

    Create cars.js

---
+++ express-knex-crud/db/cars.js
@@ -0,0 +1,25 @@
+const knex = require("./knex");
+
+function createCar(car) {
+    return knex("cars").insert(car);
+};
+
+function getAllCars() {
+    console.log(knex("cars"));
+    return knex("cars").select("");
+};
+
+function deleteCar(id) {
+    return knex("cars").where("id", id).del();
+};
+
+function updateCar(id, car) {
+    return knex("cars").where("id", id).update(car);
+};
+
+module.exports = {
+    createCar,
+    getAllCars,
+    deleteCar,
+    updateCar
+}
